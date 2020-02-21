9daa6719c8afaee94f5aab32953aefb41ab74826
Larry <lukaoma@utexas.edu>
2020-02-21 12:14:28 -0600

    fixed errors :)

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import HomePage from './Home'
@@ -31,44 +32,43 @@ function App() {
 //     fetchData();
 //   }, [fetchData]);
 
-  return (
-      < div > < style
-  lang = 'scss' / >
-      < HomePage / >
-      < /div>
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       { process.env.NODE_ENV === 'production' ?
-  //           <p>
-  //             This is a production build feerom create-react-apprrr.
-  //           </p>
-  //         : <p>
-  //             Edit <code>src/App.js</code> and save to reload.
-  //           </p>
-  //       }
-  //       <p>{'« '}<strong>
-  //         {isFetching
-  //           ? 'Fetching message from API'
-  //           : message}
-  //       </strong>{' »'}</p>
-  //       <p><a
-  //         className="App-link"
-  //         href="https://github.com/mars/heroku-cra-node"
-  //       >
-  //         React + Node deployment on Heroku
-  //       </a></p>
-  //       <p><a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a></p>
-  //     </header>
-  //   </div>
-)
-  ;
+    return (
+        <div>
+            < style lang='scss'/>
+            <HomePage/>
+        </div>
+        //     <header className="App-header">
+        //       <img src={logo} className="App-logo" alt="logo" />
+        //       { process.env.NODE_ENV === 'production' ?
+        //           <p>
+        //             This is a production build feerom create-react-apprrr.
+        //           </p>
+        //         : <p>
+        //             Edit <code>src/App.js</code> and save to reload.
+        //           </p>
+        //       }
+        //       <p>{'« '}<strong>
+        //         {isFetching
+        //           ? 'Fetching message from API'
+        //           : message}
+        //       </strong>{' »'}</p>
+        //       <p><a
+        //         className="App-link"
+        //         href="https://github.com/mars/heroku-cra-node"
+        //       >
+        //         React + Node deployment on Heroku
+        //       </a></p>
+        //       <p><a
+        //         className="App-link"
+        //         href="https://reactjs.org"
+        //         target="_blank"
+        //         rel="noopener noreferrer"
+        //       >
+        //         Learn React
+        //       </a></p>
+        //     </header>
+        //   </div>
+    );
 
 }
 
--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -2,9 +2,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import React from "react";
 
 
-ReactDOM.render( < App / >, document.getElementById('root')
+ReactDOM.render(<App/>, document.getElementById('root')
 )
 ;
 
