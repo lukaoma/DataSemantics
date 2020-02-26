348c3583791406a98d6222ed869cc7013ccd0a35
Larry <lukaoma@utexas.edu>
2020-02-26 01:21:17 -0600

    should work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import requests
 app = Flask(__name__, static_folder="react-ui/build/static", template_folder="react-ui/build")
 
 
@@ -12,7 +12,12 @@ def index():
 def hello():
     return "Hello World!!!!!!!!!!!!!!!!!!!!!!!"
 
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=5000, threaded=True)
-    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+	print("THINGS ARE RUNNING")
+    app.run(host='0.0.0.0', port=5000, threaded=True)
+    #app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
