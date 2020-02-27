5aebfa7944fdef1fe458d919f3c792f8fc52f9a6
Larry <lukaoma@utexas.edu>
2020-02-27 00:32:47 -0600

    please boo

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, send_from_directory, request
-from joblib import load
+from flask import Flask, render_template, request
 from server.explore_admission_notes import doIT
 from server.explore_admission_notes import predict
 
 app = Flask(__name__, static_folder="react-ui/build/static", template_folder="react-ui/build")
 
+model = None
+
+
+@app.route("/predict")
+def prediction():
+    global model
+    if model == None:
+        model = doIT()
+    info = request.headers.get('info')
+    return str(predict(model, info))
 
 
 @app.route("/")
@@ -17,14 +26,6 @@ def hello():
     return "Hello World!!!!!!!!!!!!!!!!!!!!!!!"
 
 
-@app.route("/predict")
-def prediction():
-    info = request.headers.get('info')
-    return str(predict(app.MLmodel, info))
-
-
 if __name__ == "__main__":
-    model = doIT()
-    app.MLmodel = model
     app.run(host='0.0.0.0', port=5000, threaded=True)
     # app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
